0cfceb5aff73f1740e4068ee5a5621520eb48624
Mas5ive <empty@empty>
2025-01-29 15:02:50 +0300

    chore(backend): configure the database

---
+++ backend/app/api/deps.py
@@ -0,0 +1,13 @@
+from typing import Annotated, Generator
+
+from app.core.db import engine
+from fastapi import Depends
+from sqlmodel import Session
+
+
+def get_db() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_db)]
---
+++ backend/app/core/db.py
@@ -0,0 +1,5 @@
+
+from app.core.config import settings
+from sqlmodel import create_engine
+
+engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
