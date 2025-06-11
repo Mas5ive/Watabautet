f3efc6fbd21ccdb763cf60cd7ad76620b5da979a
Mas5ive <empty@empty>
2025-06-11 18:02:01 +0300

    test(backend): add POST /users/signup

--- backend/app/tests/api/routes/test_users.py
+++ backend/app/tests/api/routes/test_users.py
@@ -1,6 +1,8 @@
+import pytest
 from app.core.config import settings
 from fastapi import status
 from fastapi.testclient import TestClient
+from sqlmodel import Session, delete, select
 
 API_BASE_URL = f'{settings.API_V1_STR}/users'
 
@@ -21,5 +23,32 @@ class TestReadUserMe:
     def test_read_unauthenticated_user(self, client: TestClient) -> None:
         request = client.get(self.API_ENDPOINT)
         assert request.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+class TestRegisterUser:
+
+    API_ENDPOINT = f'{API_BASE_URL}/signup'
+
+    @pytest.fixture()
+    def data_for_register(self, db: Session):
+        username = 'testuser'
+        password = 'password'
+        yield {'name': username, 'password': password}
+        db.exec(delete(User).where(User.name == username))
+        db.commit()
+
+    def test_register_new_user(self, client: TestClient, db: Session, data_for_register: dict[str, str]) -> None:
+        request = client.post(self.API_ENDPOINT, json=data_for_register)
+        assert request.status_code == status.HTTP_201_CREATED
+        response = request.json()
+        assert response['name'] == data_for_register['name']
+        user = db.exec(select(User).where(User.name == data_for_register['name'])).first()
+        assert user
+
+    def test_register_existing_user(self, client: TestClient, userdata: dict[str, str]) -> None:
+        request = client.post(self.API_ENDPOINT, json=userdata)
+        assert request.status_code == status.HTTP_400_BAD_REQUEST
+        response = request.json()
+        assert response['detail'] == 'The user with this name already exists in the system'
 
 
