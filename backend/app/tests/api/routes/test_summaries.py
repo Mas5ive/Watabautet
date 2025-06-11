d10add6f81311c3aa844e2dec6e8f5431f8c8523
Mas5ive <empty@empty>
2025-06-11 18:59:09 +0300

    test(backend): add POST /summaries/process

--- backend/app/tests/api/routes/test_summaries.py
+++ backend/app/tests/api/routes/test_summaries.py
@@ -189,3 +189,235 @@ class TestSaveSummary:
         assert request.status_code == status.HTTP_401_UNAUTHORIZED
 
 
+class TestCreateTaskSummary:
+
+    API_ENDPOINT = f'{API_BASE_URL}/process'
+
+    @pytest.fixture
+    def db_video(self, db: Session):
+        yield t_utils.create_video_in_db(session=db, link=VIDEO_PARAMS['link'])
+        db.exec(delete(Video))
+        db.commit()
+
+    @pytest.fixture
+    def db_summary(self, db: Session):
+        yield t_utils.create_summary_in_db(session=db, **SUMMARY_PARAMS,  **COMMON_SUMMARY_ATTRIBUTES)
+        db.exec(delete(Summary))
+        db.commit()
+
+    @pytest.fixture
+    def cache_summary(self):
+        return t_utils.create_item_in_cache
+
+    @pytest.fixture
+    def cache_video(self):
+        yield t_utils.create_item_in_cache
+
+    @pytest.fixture(autouse=True)
+    def cleanup_cache(self, cache: Redis):
+        yield
+        cache.flushall()
+
+    def test_create_when_parent_video_is_in_cache_with_status_success(
+        self, cache: Redis, client: TestClient, user_token_headers: dict[str, str], cache_video, task_queue
+    ) -> None:
+        cache_video(
+            cache=cache,
+            task_id=utils.TaskIdVideo.generate(**VIDEO_PARAMS),
+            status=TaskStatus.SUCCESS,
+            result=COMMON_VIDEO_ATTRIBUTES,
+            date_done='2025-03-26T19:13:53.395702+00:00'
+        )
+        request = client.post(
+            self.API_ENDPOINT,
+            headers=user_token_headers,
+            json={'summary_request': SUMMARY_PARAMS, 'video_request': VIDEO_PARAMS}
+        )
+        assert request.status_code == status.HTTP_202_ACCEPTED
+        response = request.json()
+        assert response['message'] == 'The task has been created!'
+        summary_data, video_data = t_utils.get_data_from_message(task_queue)
+        assert summary_data['language'] == SUMMARY_PARAMS['language']
+        assert summary_data['size'] == SUMMARY_PARAMS['size']
+        assert video_data == {**VIDEO_PARAMS,  **COMMON_VIDEO_ATTRIBUTES}
+        task_id = utils.TaskIdSummary.generate(**SUMMARY_PARAMS)
+        task_result = utils.get_task_result(cache=cache, task_id=task_id)
+        assert task_result is not None
+        assert task_result['status'] == TaskStatus.PENDING
+        assert task_result['result'] is None
+
+    def test_create_when_parent_video_is_in_cache_with_non_success_status(
+        self, cache: Redis, client: TestClient, user_token_headers: dict[str, str], cache_video, task_queue
+    ) -> None:
+        cache_video(
+            cache=cache,
+            task_id=utils.TaskIdVideo.generate(**VIDEO_PARAMS),
+            status=TaskStatus.PENDING,
+        )
+        request = client.post(
+            self.API_ENDPOINT,
+            headers=user_token_headers,
+            json={'summary_request': SUMMARY_PARAMS, 'video_request': VIDEO_PARAMS}
+        )
+        assert request.status_code == status.HTTP_400_BAD_REQUEST
+        response = request.json()
+        assert response['message'] == 'There is no video data for this summary'
+        message_data = t_utils.get_data_from_message(task_queue)
+        assert message_data is None
+        task_id = utils.TaskIdSummary.generate(**SUMMARY_PARAMS)
+        task_result = utils.get_task_result(cache=cache, task_id=task_id)
+        assert task_result is None
+
+    def test_create_when_parent_video_is_in_db(
+        self, cache: Redis, client: TestClient, user_token_headers: dict[str, str], db_video, task_queue
+    ) -> None:
+        request = client.post(
+            self.API_ENDPOINT,
+            headers=user_token_headers,
+            json={'summary_request': SUMMARY_PARAMS, 'video_request': VIDEO_PARAMS}
+        )
+        assert request.status_code == status.HTTP_202_ACCEPTED
+        response = request.json()
+        assert response['message'] == 'The task has been created!'
+        summary_data, video_data = t_utils.get_data_from_message(task_queue)
+        assert summary_data['language'] == SUMMARY_PARAMS['language']
+        assert summary_data['size'] == SUMMARY_PARAMS['size']
+        assert video_data == {**VIDEO_PARAMS,  **COMMON_VIDEO_ATTRIBUTES}
+        task_id = utils.TaskIdSummary.generate(**SUMMARY_PARAMS)
+        task_result = utils.get_task_result(cache=cache, task_id=task_id)
+        assert task_result is not None
+        assert task_result['status'] == TaskStatus.PENDING
+        assert task_result['result'] is None
+
+    def test_create_when_parent_video_does_not_exist(
+        self, cache: Redis, client: TestClient, user_token_headers: dict[str, str], task_queue
+    ) -> None:
+        request = client.post(
+            self.API_ENDPOINT,
+            headers=user_token_headers,
+            json={'summary_request': SUMMARY_PARAMS, 'video_request': VIDEO_PARAMS}
+        )
+        assert request.status_code == status.HTTP_400_BAD_REQUEST
+        response = request.json()
+        assert response['message'] == 'There is no video data for this summary'
+        message_data = t_utils.get_data_from_message(task_queue)
+        assert message_data is None
+        task_id = utils.TaskIdSummary.generate(**SUMMARY_PARAMS)
+        task_result = utils.get_task_result(cache=cache, task_id=task_id)
+        assert task_result is None
+
+    def test_create_when_task_is_in_cache_with_status_pending(
+        self, cache: Redis, client: Redis, user_token_headers: dict[str, str], db_video, cache_summary, task_queue
+    ) -> None:
+        cache_summary(
+            cache=cache,
+            task_id=utils.TaskIdSummary.generate(**SUMMARY_PARAMS),
+            status=TaskStatus.PENDING
+        )
+        request = client.post(
+            self.API_ENDPOINT,
+            headers=user_token_headers,
+            json={'summary_request': SUMMARY_PARAMS, 'video_request': VIDEO_PARAMS}
+        )
+        assert request.status_code == status.HTTP_400_BAD_REQUEST
+        response = request.json()
+        assert response['message'] == 'The task already exists'
+        message_data = t_utils.get_data_from_message(task_queue)
+        assert message_data is None
+
+    def test_create_when_task_is_in_cache_with_status_success(
+        self, cache: Redis, client: Redis, user_token_headers: dict[str, str], db_video, cache_summary, task_queue
+    ) -> None:
+        cache_summary(
+            cache=cache,
+            task_id=utils.TaskIdSummary.generate(**SUMMARY_PARAMS),
+            status=TaskStatus.SUCCESS,
+            result=COMMON_SUMMARY_ATTRIBUTES,
+            date_done='2025-03-26T19:13:53.395702+00:00'
+        )
+        request = client.post(
+            self.API_ENDPOINT,
+            headers=user_token_headers,
+            json={'summary_request': SUMMARY_PARAMS, 'video_request': VIDEO_PARAMS}
+        )
+        assert request.status_code == status.HTTP_400_BAD_REQUEST
+        response = request.json()
+        assert response['message'] == 'The summary is already in the cache'
+        message_data = t_utils.get_data_from_message(task_queue)
+        assert message_data is None
+
+    def test_create_when_task_is_in_cache_with_status_failed(
+            self, cache: Redis, client: TestClient, user_token_headers: dict[str, str], db_video, cache_summary,
+            task_queue
+    ) -> None:
+        cache_summary(
+            cache=cache,
+            task_id=utils.TaskIdSummary.generate(**SUMMARY_PARAMS),
+            status=TaskStatus.FAILURE,
+            result={
+                'exc_type': 'Exception',
+                'exc_message': [],
+                'exc_module': 'builtins'
+            },
+            date_done=t_utils.get_formatted_time_offset()
+        )
+        request = client.post(
+            self.API_ENDPOINT,
+            headers=user_token_headers,
+            json={'summary_request': SUMMARY_PARAMS, 'video_request': VIDEO_PARAMS}
+        )
+        assert request.status_code == status.HTTP_503_SERVICE_UNAVAILABLE
+        response = request.json()
+        assert response['message'] == 'Some service is not working properly or is busy. Try the request again later'
+        assert 'Retry-After' in request.headers
+        message_data = t_utils.get_data_from_message(task_queue)
+        assert message_data is None
+
+    def test_create_when_task_is_in_cache_with_status_failed_but_cooled_down(
+            self, cache: Redis, client: TestClient, user_token_headers: dict[str, str], cache_summary,
+            task_queue, db_video
+    ) -> None:
+        cache_summary(
+            cache=cache,
+            task_id=utils.TaskIdSummary.generate(**SUMMARY_PARAMS),
+            status=TaskStatus.FAILURE,
+            result={
+                'exc_type': 'Exception',
+                'exc_message': [],
+                'exc_module': 'builtins'
+            },
+            date_done=t_utils.get_formatted_time_offset(offset=-settings.FAILURE_COOLDOWN_SEC)
+        )
+        request = client.post(
+            self.API_ENDPOINT,
+            headers=user_token_headers,
+            json={'summary_request': SUMMARY_PARAMS, 'video_request': VIDEO_PARAMS}
+        )
+        assert request.status_code == status.HTTP_202_ACCEPTED
+        response = request.json()
+        assert response['message'] == 'The task has been created!'
+        summary_data, video_data = t_utils.get_data_from_message(task_queue)
+        assert summary_data['language'] == SUMMARY_PARAMS['language']
+        assert summary_data['size'] == SUMMARY_PARAMS['size']
+        assert video_data == {**VIDEO_PARAMS,  **COMMON_VIDEO_ATTRIBUTES}
+
+    def test_create_for_existing_summary_in_db(
+            self, client: TestClient, user_token_headers: dict[str, str], db_video, db_summary, task_queue
+    ) -> None:
+        request = client.post(
+            self.API_ENDPOINT,
+            headers=user_token_headers,
+            json={'summary_request': SUMMARY_PARAMS, 'video_request': VIDEO_PARAMS}
+        )
+        assert request.status_code == status.HTTP_400_BAD_REQUEST
+        response = request.json()
+        assert response['message'] == 'The summary is already in the DB'
+        message_data = t_utils.get_data_from_message(task_queue)
+        assert message_data is None
+
+    def test_create_for_unauthenticated_user(self, client: TestClient) -> None:
+        request = client.post(
+            self.API_ENDPOINT,
+            json={'summary_request': SUMMARY_PARAMS, 'video_request': VIDEO_PARAMS}
+        )
+        assert request.status_code == status.HTTP_401_UNAUTHORIZED
