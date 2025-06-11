af9f7fa910a9459ace69093ceb025aef986edaae
Mas5ive <empty@empty>
2025-06-11 18:28:45 +0300

    test(backend): add GET /videos

---
+++ backend/app/tests/api/routes/test_videos.py
@@ -0,0 +1,68 @@
+import app.api.utils as utils
+import app.tests.utils as t_utils
+import pytest
+from app.core.config import settings
+from app.models import TaskStatus, Video
+from fastapi import status
+from fastapi.testclient import TestClient
+from redis import Redis
+from sqlmodel import Session, delete
+
+API_BASE_URL = f'{settings.API_V1_STR}/videos'
+
+
+VIDEO_PARAMS = {
+    'link': 'q' * 11,
+    'major_language': 'ru'
+}
+COMMON_VIDEO_ATTRIBUTES = {
+    'description': 'bla-bla',
+    'text': 'bla-bla',
+    'category': 'comedy',
+    'title': 'wow'
+}
+
+
+class TestGetVideo:
+
+    API_ENDPOINT = f'{API_BASE_URL}/'
+
+    @pytest.fixture
+    def db_video(self, db: Session):
+        yield t_utils.create_video_in_db(session=db, **VIDEO_PARAMS, **COMMON_VIDEO_ATTRIBUTES)
+        db.exec(delete(Video))
+        db.commit()
+
+    @pytest.fixture
+    def cache_video(self, cache: Redis):
+        yield t_utils.create_item_in_cache(
+            cache=cache,
+            task_id=utils.TaskIdVideo.generate(**VIDEO_PARAMS),
+            status=TaskStatus.SUCCESS,
+            result=COMMON_VIDEO_ATTRIBUTES,
+            date_done='2025-03-26T19:13:53.395702+00:00'
+        )
+        cache.flushall()
+
+    def test_get_from_cache(self, client: TestClient, user_token_headers: dict[str, str], cache_video) -> None:
+        request = client.get(self.API_ENDPOINT, params=VIDEO_PARAMS, headers=user_token_headers)
+        assert request.status_code == status.HTTP_200_OK
+        response = request.json()
+        assert response['category'] == COMMON_VIDEO_ATTRIBUTES['category']
+        assert response['link'] == VIDEO_PARAMS['link']
+
+    def test_get_from_db(self, client: TestClient, user_token_headers: dict[str, str], db_video) -> None:
+        request = client.get(self.API_ENDPOINT, params=VIDEO_PARAMS, headers=user_token_headers)
+        assert request.status_code == status.HTTP_200_OK
+        response = request.json()
+        assert response['category'] == COMMON_VIDEO_ATTRIBUTES['category']
+        assert response['link'] == VIDEO_PARAMS['link']
+
+    def test_not_found(self, client: TestClient, user_token_headers: dict[str, str]) -> None:
+        non_existent_video_params = {'link': 'x' * 11, 'major_language': 'en'}
+        request = client.get(self.API_ENDPOINT, params=non_existent_video_params, headers=user_token_headers)
+        assert request.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_get_for_unauthenticated_user(self, client: TestClient) -> None:
+        request = client.get(self.API_ENDPOINT, params=VIDEO_PARAMS)
+        assert request.status_code == status.HTTP_401_UNAUTHORIZED
--- backend/app/tests/utils.py
+++ backend/app/tests/utils.py
@@ -1,5 +1,9 @@
+import json
 from app import crud
+from app.core.config import settings
 from app.models import Summary, Video
+from app.models import TaskStatus
+from redis import Redis
 from sqlmodel import Session
 
 VIDEO_LINK = 'v' * 11
@@ -47,5 +51,74 @@ def create_summary_in_db(
         text=text
     ))
     return summary
+
+
+def create_item_in_cache(
+    cache: Redis,
+    task_id: str,
+    status: TaskStatus = TaskStatus.PENDING,
+    result: dict | None = None,
+    traceback: str | None = None,
+    children: list | None = None,
+    date_done: str | None = None
+
+) -> None:
+    """
+    Creates a task item in the Redis cache with the specified status and data.
+
+    Note:
+
+    Some examples showing possible data in the cache:
+
+    PENDING:
+    ```{
+        "status": "PENDING",
+        "result": null,
+        "traceback": null,
+        "children": null,
+        "date_done": null,
+        "task_id": "32fb09c1-0317-538e-9a06-9c6f3ceabe18"
+    }
+    ```
+
+    FAILURE:
+    ```{
+        "status": "FAILURE",
+        "result": {
+            "exc_type": "Exception",
+            "exc_message": [],
+            "exc_module": "builtins"
+        },
+        "traceback": "Traceback (most recent call last):  File /workers/.venv/lib/python3.13...",
+        "children": [],
+        "date_done": "2022-03-26T19:08:35.809483+00:00",
+        "task_id": "32fb09c1-0317-538e-9a06-9c6f3ceabe18"
+    }
+    ```
+
+    SUCCESS:
+    ```{
+        "status": "SUCCESS",
+        "result": {
+            "field1": "text",
+            "field2": "text"
+        },
+        "traceback": null,
+        "children": [],
+        "date_done": "2022-03-26T19:13:53.395702+00:00",
+        "task_id": "32fb09c1-0317-538e-9a06-9c6f3ceabe18"
+    }
+    ```
+    """
+    cache_key = settings.CACHE_KEY_PREFIX + task_id
+    data = {
+        'status': status,
+        'result': result,
+        'traceback': traceback,
+        'children': children,
+        'date_done': date_done,
+        'task_id': task_id
+    }
+    cache.set(cache_key, json.dumps(data))
 
 
