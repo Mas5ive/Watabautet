9592e60c22176276ae22a56f4b891e648efcb5ed
Mas5ive <empty@empty>
2025-06-11 18:30:09 +0300

    test(backend): add POST /videos/store

--- backend/app/tests/api/routes/test_videos.py
+++ backend/app/tests/api/routes/test_videos.py
@@ -66,3 +66,85 @@ class TestGetVideo:
     def test_get_for_unauthenticated_user(self, client: TestClient) -> None:
         request = client.get(self.API_ENDPOINT, params=VIDEO_PARAMS)
         assert request.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+class TestSaveVideo:
+
+    API_ENDPOINT = f'{API_BASE_URL}/store'
+
+    @pytest.fixture
+    def db_video(self, db: Session):
+        return t_utils.create_video_in_db(session=db, **VIDEO_PARAMS, **COMMON_VIDEO_ATTRIBUTES)
+
+    @pytest.fixture(autouse=True)
+    def delete_db_video(self, db: Session):
+        yield
+        db.exec(delete(Video))
+        db.commit()
+
+    @pytest.fixture
+    def cache_video(self, cache: Redis):
+        yield t_utils.create_item_in_cache
+        cache.flushall()
+
+    def test_save_video(
+            self, db: Session, cache: Redis, client: TestClient, user_token_headers: dict[str, str], cache_video
+    ) -> None:
+        cache_video(
+            cache=cache,
+            task_id=utils.TaskIdVideo.generate(**VIDEO_PARAMS),
+            status=TaskStatus.SUCCESS,
+            result=COMMON_VIDEO_ATTRIBUTES,
+            date_done='2025-03-26T19:13:53.395702+00:00'
+        )
+        request = client.post(self.API_ENDPOINT, headers=user_token_headers, json=VIDEO_PARAMS)
+        assert request.status_code == status.HTTP_201_CREATED
+        response = request.json()
+        assert response['message'] == 'The video successfully saved'
+        video = db.get(Video, VIDEO_PARAMS['link'])
+        assert video is not None
+        assert video.link == VIDEO_PARAMS['link']
+        assert video.category == COMMON_VIDEO_ATTRIBUTES['category']
+
+    def test_save_exixisting_video_in_db(
+            self, cache: Redis, client: TestClient, user_token_headers: dict[str, str], cache_video, db_video
+    ) -> None:
+        cache_video(
+            cache=cache,
+            task_id=utils.TaskIdVideo.generate(**VIDEO_PARAMS),
+            status=TaskStatus.SUCCESS,
+            result=COMMON_VIDEO_ATTRIBUTES,
+            date_done='2025-03-26T19:13:53.395702+00:00'
+        )
+        request = client.post(self.API_ENDPOINT, headers=user_token_headers, json=VIDEO_PARAMS)
+        assert request.status_code == status.HTTP_200_OK
+        response = request.json()
+        assert response['message'] == 'The video has already been saved'
+
+    def test_not_found(self, client: TestClient, user_token_headers: dict[str, str]) -> None:
+        non_existent_video_params = {'link': 'x' * 11, 'major_language': 'en'}
+        request = client.post(self.API_ENDPOINT, headers=user_token_headers, json=non_existent_video_params)
+        assert request.status_code == status.HTTP_404_NOT_FOUND
+        response = request.json()
+        assert response['message'] == 'The video was not found in the cache'
+
+    def test_save_unsuccessful_task(
+            self, cache: Redis, client: TestClient, user_token_headers: dict[str, str], cache_video
+    ) -> None:
+        cache_video(
+            cache=cache,
+            task_id=utils.TaskIdVideo.generate(**VIDEO_PARAMS),
+            status=TaskStatus.PENDING,
+            result=None,
+            date_done=None
+        )
+        request = client.post(self.API_ENDPOINT, headers=user_token_headers, json=VIDEO_PARAMS)
+        assert request.status_code == status.HTTP_400_BAD_REQUEST
+        response = request.json()
+        assert response['message'] == 'The video must be complete!'
+
+    def test_save_for_unauthenticated_user(self, client: TestClient) -> None:
+        request = client.post(self.API_ENDPOINT, json=VIDEO_PARAMS)
+        assert request.status_code == status.HTTP_401_UNAUTHORIZED
+
+
