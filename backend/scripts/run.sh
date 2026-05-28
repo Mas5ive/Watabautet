ab4fc523a784c1414ae2b6a057dce922eee6628b
Mas5ive <empty@empty>
2026-05-29 01:00:15 +0300

    build: add prometheus

--- backend/scripts/run.sh
+++ backend/scripts/run.sh
@@ -3,6 +3,10 @@
 set -e
 set -x
 
+# Create and clean up a folder for Prometheus multi-process metrics
+rm -rf /tmp/prometheus_multiproc_dir
+mkdir -p /tmp/prometheus_multiproc_dir
+
 alembic upgrade head
 
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --workers 4
